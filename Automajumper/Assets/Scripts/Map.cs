b26407ad9dc7c7b675d4b7e6569798cc894e09a2
Yuxiang-Huang <yuxiangh142857@gmail.com>
2023-09-16 19:22:28 -0400

    Make no change to the map and instead convert indices to world positions now

--- Automajumper/Assets/Scripts/Map.cs
+++ Automajumper/Assets/Scripts/Map.cs
@@ -10,73 +10,100 @@ public class Map : MonoBehaviour
     public static Map instance;
 
     public int[,] map;
-    public GameObject[,] cubes = new GameObject[10, 10];
+    public GameObject[,] cubes;
 
-    [SerializeField] float transitionTime = 1f;
+    [SerializeField] float transitionTime;
+    [SerializeField] float curTime;
 
     void Awake()
     {
         instance = this;
 
+        // test
+        //map = new int[,] {
+        //    { 0, 0, 0, 0, 0, 0 },
+        //    { 0, 1, 1, 1, 0, 0 },
+        //    { 0, 0, 0, 0, 0, 0 },
+        //    { 0, 0, 0, 0, 0, 0 },   
+        //};
+
         // initialize the map
-        map = new int[,] {
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            { 1, 1, 1, 0, 0, 0, 0, 0, 0, 0},
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            { 0, 0, 0, 0, 0, 0, 1, 1, 1, 0},
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        };
+        map = new int[20, 20];
 
-        // transpose the map
-        int[,] newMap = new int[map.GetLength(1), map.GetLength(0)];
+        cubes = new GameObject[map.GetLength(0), map.GetLength(1)];
 
-        for (int i = 0; i < map.GetLength(0); i++)
-        {
-            for (int j = 0; j < map.GetLength(1); j++)
-            {
-                newMap[j, i] = map[i, j];
-            }
-        }
-        map = newMap;
+        // camera position and size
+        Camera.main.transform.position = new Vector3(map.GetLength(1) / 2, map.GetLength(0) / 2, -10);
+        Camera.main.orthographicSize = Mathf.Max(map.GetLength(1) / 2, map.GetLength(0) / 2);
+
+        // create the patterns
+        CreateBlock(map, 10, 2);
+        CreateBlock(map, 10, 5);
+
+        // transpose the map and reflect it across the middle line
+        //int[,] newMap = new int[map.GetLength(1), map.GetLength(0)];
+
+        //for (int i = 0; i < map.GetLength(0); i++)
+        //{
+        //    for (int j = 0; j < map.GetLength(1); j++)
+        //    {
+        //        newMap[j, map.GetLength(0) - i - 1] = map[i, j];
+        //    }
+        //}
+        //map = newMap;
 
         // generate the cubes
         for (int i = 0; i < map.GetLength(0); i++)
         {
             for (int j = 0; j < map.GetLength(1); j++)
             {
-                if (newMap[i, j] == 1)
+                if (map[i, j] == 1)
                 {
-                    cubes[i, j] = Instantiate(cube, new Vector3(i, j, 0), Quaternion.identity);
+                    cubes[i, j] = Instantiate(cube, GetWorldPosFromArrayIndices(i, j), Quaternion.identity);
                 }
             }
         }
 
-        // generate the lines
+        // generate the lines of the grid
+        GameObject grid = new GameObject("Grid");
+
+        // horizontal lines
         for (int i = 0; i < map.GetLength(0); i++)
         {
-            Instantiate(line, new Vector3(i + 0.5f, 0, 0), Quaternion.Euler(0, 0, 90));
+            Instantiate(line, new Vector3(0, i + 0.5f, 0), Quaternion.identity, grid.transform);
         }
-        Instantiate(line, new Vector3(-0.5f, 0, 0), Quaternion.Euler(0, 0, 90));
-   
+        Instantiate(line, new Vector3(0, -0.5f, 0), Quaternion.identity, grid.transform);
 
-        for (int j = 0; j < map.GetLength(1); j++)
+        // vertical lines
+        for (int i = 0; i < map.GetLength(1); i++)
         {
-            Instantiate(line, new Vector3(0, j + 0.5f, 0), Quaternion.identity);
+           Instantiate(line, new Vector3(i + 0.5f, 0, 0), Quaternion.Euler(0, 0, 90), grid.transform);
         }
-        Instantiate(line, new Vector3(0, -0.5f, 0), Quaternion.identity);
+        Instantiate(line, new Vector3(-0.5f, 0, 0), Quaternion.Euler(0, 0, 90), grid.transform);
 
-        transitionTime = 1f;
+        // start the time
+        curTime = transitionTime;
     }
+
+    private void CreateBlock(int[,] map, int i, int j)
+    {
+        map[i, j] = 1;
+        map[i + 1, j] = 1;
+        map[i, j + 1] = 1;
+        map[i + 1, j + 1] = 1;
+    }
+
 
     private void Update()
     {
-        transitionTime -= Time.deltaTime;
+        curTime -= Time.deltaTime;
 
-        if (transitionTime < 0)
+        // for every transition time
+        if (curTime < 0)
         {
-            transitionTime = 1f;
+            curTime = transitionTime;
 
+            // update the map
             int[,] newMap = new int[map.GetLength(0), map.GetLength(1)];
 
             for (int i = 0; i < map.GetLength(0); i++)
@@ -100,7 +127,7 @@ public class Map : MonoBehaviour
                     {
                         if (CountAliveNeighbors(map, i, j) == 3)
                         {
-                            cubes[i, j] = Instantiate(cube, new Vector3(i, j, 0), Quaternion.identity);
+                            cubes[i, j] = Instantiate(cube, GetWorldPosFromArrayIndices(i, j), Quaternion.identity);
                             newMap[i, j] = 1;
                         }
                     }
@@ -111,6 +138,13 @@ public class Map : MonoBehaviour
         }
     }
 
+    Vector3 GetWorldPosFromArrayIndices(int i, int j)
+    {
+        // transpose the map and reflect it across the middle line
+        return new Vector3(j, map.GetLength(0) - i - 1, 0);
+    }
+
+    // count the alive neighbors of this cell in coordinate i, j in the map
     int CountAliveNeighbors(int[,] map, int i, int j)
     {
         int ans = 0;
@@ -132,7 +166,7 @@ public class Map : MonoBehaviour
         }
 
         // top three neighbors
-        if (i + 1 < map.GetLength(1))
+        if (i + 1 < map.GetLength(0))
         {
             if (map[i + 1, j] == 1)
                 ans++;
