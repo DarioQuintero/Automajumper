829f54617f992f8e75532d9aabeb8acc5002885a
Gabe <gabriel.arias@edificiosanluis.com>
2023-10-28 18:54:56 -0400

    Added level select btn

--- Automajumper/Assets/Scripts/UI/TitleScreen.cs
+++ Automajumper/Assets/Scripts/UI/TitleScreen.cs
@@ -11,9 +11,9 @@ public class TitleScreen : MonoBehaviour
     private void OnEnable()
     {
         VisualElement root  = GetComponent<UIDocument>().rootVisualElement;
-        _navigateBtn = root.Q<Button>("navigateBtn");
+        _navigateBtn = root.Q<Button>("playBtn");
 
-        _navigateBtn.clicked += () => SceneManager.LoadScene("Level 1");
+        _navigateBtn.clicked += () => SceneManager.LoadScene("Level 1"); //TODO - call level manager script here based on 
     }
 
     // Start is called before the first frame update
