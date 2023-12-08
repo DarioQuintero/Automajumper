d03014500665802b74eb97c29602bb8b850e15be
Yuxiang-Huang <yuxiangh142857@gmail.com>
2023-12-07 23:31:49 -0500

    Improve cut scene skipping logic

--- Automajumper/Assets/Scripts/CutScreenManager.cs
+++ Automajumper/Assets/Scripts/CutScreenManager.cs
@@ -1,26 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class CutScreenManager : MonoBehaviour
 {
     [SerializeField] int levelNum;
     [SerializeField] GameObject skipInstruction;
+    [SerializeField] bool skipInstructionShown;
+    [SerializeField] int displayCount = 0;
 
     public void OnSkipForward()
     {
         SceneManager.LoadScene("Level " + (levelNum + 1));
     }
 
-    public void OnShowSkipInstruction()
+    public void OnShowSkipInstruction(InputAction.CallbackContext context)
     {
-        skipInstruction.SetActive(true);
-        Invoke(nameof(HideSkipInstruction), 3f);
+        if (context.started)
+        {
+            skipInstruction.SetActive(true);
+            StartCoroutine(nameof(HideSkipInstruction));
+        }
     }
 
-    void HideSkipInstruction()
+    IEnumerator HideSkipInstruction()
     {
-        skipInstruction.SetActive(false);
-    } 
+        displayCount++;
+
+        yield return new WaitForSeconds(3f);
+
+        displayCount--;
+
+        if (displayCount == 0)
+            skipInstruction.SetActive(false);
+    }
 }
