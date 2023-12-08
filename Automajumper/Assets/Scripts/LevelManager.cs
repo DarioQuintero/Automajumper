3d603791bfbe6ab5f563dc7c80067921678daa90
Yuxiang-Huang <yuxiangh142857@gmail.com>
2023-12-08 02:09:56 -0500

    Destroy bounds after reaching finishline

--- Automajumper/Assets/Scripts/LevelManager.cs
+++ Automajumper/Assets/Scripts/LevelManager.cs
@@ -4,6 +4,7 @@ using Cinemachine;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using static UnityEngine.UIElements.UxmlAttributeDescription;
 
 public class LevelManager : MonoBehaviour
 {
@@ -54,6 +55,10 @@ public class LevelManager : MonoBehaviour
         else if (levelNum == 2)
         {
             yield return new WaitForSeconds(9f);
+        }
+        else if (levelNum == 3)
+        {
+            yield return new WaitForSeconds(2f);
         }
 
 
