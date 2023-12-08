34a0cbd6454cb1232c38148e07f91b4dbbac6694
Yuxiang-Huang <yuxiangh142857@gmail.com>
2023-12-07 23:17:01 -0500

    Able to display skip info in cut scene with any key

--- Automajumper/Assets/Scripts/CutScreenManager.cs
+++ Automajumper/Assets/Scripts/CutScreenManager.cs
@@ -6,9 +6,21 @@ using UnityEngine.SceneManagement;
 public class CutScreenManager : MonoBehaviour
 {
     [SerializeField] int levelNum;
+    [SerializeField] GameObject skipInstruction;
 
     public void OnSkipForward()
     {
         SceneManager.LoadScene("Level " + (levelNum + 1));
     }
+
+    public void OnShowSkipInstruction()
+    {
+        skipInstruction.SetActive(true);
+        Invoke(nameof(HideSkipInstruction), 3f);
+    }
+
+    void HideSkipInstruction()
+    {
+        skipInstruction.SetActive(false);
+    } 
 }
