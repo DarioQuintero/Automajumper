a28e5f3d83d38da72c0a61add1452d36ba1e1fe3
Yuxiang-Huang <yuxiangh142857@gmail.com>
2023-12-07 20:24:28 -0500

    Fade in works for PLC screen

--- Automajumper/Assets/Scripts/PLCManager.cs
+++ Automajumper/Assets/Scripts/PLCManager.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PLCManager : MonoBehaviour
 {
     [SerializeField] RawImage levelTransitionImage;
+
+    [SerializeField] List<Image> buttonImages;
+    [SerializeField] List<TextMeshProUGUI> texts;
 
     // Start is called before the first frame update
     void Start()
@@ -15,13 +19,19 @@ public class PLCManager : MonoBehaviour
 
     IEnumerator FadeIn()
     {
-        levelTransitionImage.gameObject.SetActive(true);
         int steps = 100;
         for (int i = 0; i < steps; i++)
         {
             yield return new WaitForSeconds(1f / steps);
-            levelTransitionImage.color = new Color(0, 0, 0, 1 - i / 100f);
+            foreach (Image buttonImage in buttonImages)
+            {
+                buttonImage.color = new Color(255, 255, 255, i / 100f);
+            }
+
+            foreach (TextMeshProUGUI text in texts)
+            {
+                text.color = new Color(text.color.r, text.color.g, text.color.b, i / 100f);
+            }
         }
-        levelTransitionImage.gameObject.SetActive(false);
     }
 }
