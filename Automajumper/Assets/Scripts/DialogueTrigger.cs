739743dba17ffd715f44e630309b4ab6f693c38f
Anthony Yip <anthony.ch.yip@gmail.com>
2023-12-07 22:05:31 -0500

    Dialogue working

--- Automajumper/Assets/Scripts/DialogueTrigger.cs
+++ Automajumper/Assets/Scripts/DialogueTrigger.cs
@@ -12,6 +12,7 @@ public class DialogueTrigger : MonoBehaviour
         GameObject canvas = GameObject.Find("Canvas");
         dialogueObject = canvas.transform.Find("Dialogue").gameObject;
         textMesh = dialogueObject.transform.Find("Text").gameObject.GetComponent<TextMeshProUGUI>();
+        dialogueObject.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -19,14 +20,44 @@ public class DialogueTrigger : MonoBehaviour
         if (other.gameObject.CompareTag("Dialogue"))
         {
             // Display custom text
-            textMesh.text = other.GetComponent<DialogueString>().dialogueString;
+            string fullText = other.GetComponent<DialogueString>().dialogueString;
             Destroy(other.gameObject);
+
+            string[] splitText = fullText.Split("$");
             // Show dialogue object
             dialogueObject.SetActive(true);
+            StartCoroutine(DisplayLinesOverTime(splitText));
+
+
+            // foreach (string text in splitText)
+            // {
+            //     textMesh.text = text;
+            //     // Hide dialogue object after a delay (e.g., 3 seconds)
+            // }
             
-            // Hide dialogue object after a delay (e.g., 3 seconds)
-            Invoke("HideDialogue", 3f);
         }
+    }
+
+    IEnumerator DisplayLinesOverTime(string[] lines)
+    {
+        for (int i = 0; i < lines.Length; i++)
+        {
+            textMesh.text = lines[i];
+            yield return new WaitForSeconds(3f);
+
+            // Check if this is the last line
+            if (i == lines.Length - 1)
+            {
+                HideDialogue(); // Invoke a function to hide the dialogue after the last line
+            }
+        }
+        
+        // foreach (string line in lines)
+        // {
+        //     textMesh.text = line; // Set the TextMesh text to the current line
+        //     yield return new WaitForSeconds(3f); // Wait for a few seconds
+        // }
+        // Invoke("HideDialogue", 3f);
     }
 
     private void HideDialogue()
